5d3af07a72c13f5cca4dc2db71b63d70b7f4e1dc
emaccheese <macias.erik@uabc.edu.mx>
2014-12-16 11:57:14 -0800

    Se compuso el Tower Defense

--- src/Interfaz/Screen.java
+++ src/Interfaz/Screen.java
@@ -106,7 +106,7 @@ public class Screen extends JPanel implements Runnable {
 		tileset_res[2] = imgRes2.getImage();
 		
 		//mob = new ImageIcon("/Datos/res/melet.png");
-		mob1 = new ImageIcon("/Datos/res/bee.png");
+		mob1 = new ImageIcon(Screen.class.getResource("/Datos/res/melet.png"));
 		tileset_mob[0] = mob1.getImage();
 		//tileset_mob[1] = mob1.getImage();
 
